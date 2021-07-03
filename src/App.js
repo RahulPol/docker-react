c35efa239932012b3570d78c14dbf7800d3099dc
RahulVPol <polrahul10@gmail.com>
2021-07-03 13:56:17 +0530

    added demo feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Well well well, look how the turn tables!!</p>
+        <p>This is a demo feature</p>
         <a
           className="App-link"
           href="https://reactjs.org"
